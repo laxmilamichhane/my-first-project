d113fa4c217b75e742c16e8654d18958389743f1
Sudha Shrestha <itsmesudha12@gmail.com>
2020-07-26 13:24:55 +0545

    Setup eslint and prettier.

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: '@react-native-community',
+  plugins: ['prettier'],
+  rules: {
+    'react-native/no-color-literals': 'off',
+    'react-native/no-inline-styles': 'off',
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  bracketSpacing: true,
+  singleQuote: true,
+  jsxBracketSameLine: true,
+  trailingComma: 'all',
+};
